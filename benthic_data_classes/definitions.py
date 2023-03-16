f59fb2b18bceb37bbc50e185848e31e5e2f39cdd
IsaacXu1024 <ixu@ualberta.ca>
2023-03-16 15:16:30 -0300

    Add files via upload
    DEV/ENH: Initial Upload

---
+++ benthic_data_classes/definitions.py
@@ -0,0 +1,175 @@
+import os
+import shutil
+import time
+import tarfile
+import torch
+
+import numpy as np
+import pandas as pd
+import PIL.Image
+import torch.utils.data
+
+class BenthicNetDatasetSSL(torch.utils.data.Dataset):
+    """BenthicNet dataset."""
+
+    def __init__(
+            self, root_dir, csv_file=None, transform=None, yield_pseudo_label=True
+    ):
+        """
+        Dataset for BenthicNet data.
+
+        Parameters
+        ----------
+        root_dir : str
+            Directory with all the images.
+        csv_file : str
+            Path to the csv file with annotations.
+        transform : callable, optional
+            Optional transform to be applied on a sample.
+        yield_pseudo_label : bool, default=True
+            Whether to yield a constant label-like int as well as the image.
+            N.B. The dataset is unlabelled so the label is always ``0``.
+        """
+        if csv_file is None:
+            csv_file = os.path.join(root_dir, "dataset.csv")
+        self.root_dir = root_dir
+        if os.path.isdir(os.path.join(root_dir, "tar")):
+            self.tar_dir = os.path.join(root_dir, "tar")
+        else:
+            self.tar_dir = root_dir
+        self.dataframe = pd.read_csv(csv_file, low_memory=False)
+        #self.dataframe = self.dataframe.head(64)
+        if "path" not in self.dataframe.columns:
+            self.dataframe["path"] = (
+                    self.dataframe["dataset"]
+                    + "/"
+                    + self.dataframe["site"]
+                    + "/"
+                    + self.dataframe["image"]
+            )
+        self.dataframe["tarname"] = self.dataframe["dataset"] + ".tar"
+        self.transform = transform
+        self.yield_pseudo_label = yield_pseudo_label
+
+    def __len__(self):
+        return len(self.dataframe)
+
+    def __getitem__(self, idx):
+        row = self.dataframe.iloc[idx]
+        start_time = time.time()
+        node_file_path = os.path.join(os.environ['SLURM_TMPDIR'], row["path"])
+        if os.path.isfile(node_file_path):
+            sample = PIL.Image.open(node_file_path)
+        else:
+            with tarfile.open(os.path.join(self.tar_dir, row["tarname"]), mode="r") as t:
+                sample = PIL.Image.open(t.extractfile(row["path"]))
+                # PIL.Image has lazy data loading. But PIL won't be able to access
+                # the data from the tarball once we've left this context, so we have
+                # to manually trigger the loading of the data now.
+                sample.load()
+                #print(time.time() - start_time)
+            node_file_temp_path = os.path.join(os.environ['SLURM_TMPDIR'], row["image"])
+            sample.save(node_file_temp_path)
+            os.makedirs(os.path.join(os.environ['SLURM_TMPDIR'], row["dataset"], row["site"]), exist_ok=True)
+            shutil.move(node_file_temp_path, node_file_path)
+        load_time = time.time() - start_time
+        #print("load time:", load_time)
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        if self.yield_pseudo_label:
+            return load_time, sample, 0
+        return sample
+
+
+class BenthicNetDataset(torch.utils.data.Dataset):
+    """BenthicNet dataset."""
+
+    def __init__(
+            self, tar_dir, annotations=None, transform=None
+    ):
+        """
+        Dataset for BenthicNet data.
+
+        Parameters
+        ----------
+        tar_dir : str
+            Directory with all the images.
+        annotations : str
+            Dataframe with annotations.
+        transform : callable, optional
+            Optional transform to be applied on a sample.
+        """
+        self.tar_dir = tar_dir
+        self.dataframe = annotations
+        # self.dataframe = self.dataframe.head(64)
+        self.dataframe["tarname"] = self.dataframe["dataset"] + ".tar"
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.dataframe)
+
+    def __getitem__(self, idx):
+        row = self.dataframe.iloc[idx]
+        start_time = time.time()
+        if type(row["image"]) is float:
+            row["image"] = row["url"].split("/")[-1]
+            print(row)
+        img_name = ".".join(row["image"].split(".")[:-1])     
+        path = row["dataset"]+"/"+row["site"]+"/"+img_name+".jpg"
+        node_file_path = os.path.join(os.environ['SLURM_TMPDIR'], path)
+        if os.path.isfile(node_file_path):
+            sample = PIL.Image.open(node_file_path)
+        else:
+            with tarfile.open(os.path.join(self.tar_dir, row["tarname"]), mode="r") as t:
+                #print(row)
+                sample = PIL.Image.open(t.extractfile(path))
+                # PIL.Image has lazy data loading. But PIL won't be able to access
+                # the data from the tarball once we've left this context, so we have
+                # to manually trigger the loading of the data now.
+                sample.load()
+                # print(time.time() - start_time)
+            node_file_temp_path = os.path.join(os.environ['SLURM_TMPDIR'], img_name+".jpg")
+            sample.save(node_file_temp_path)
+            os.makedirs(os.path.join(os.environ['SLURM_TMPDIR'], row["dataset"], row["site"]), exist_ok=True)
+            shutil.move(node_file_temp_path, node_file_path)
+        #load_time = time.time() - start_time
+        #print("load time:", load_time)
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample, row['label_id'], #path
+    
+    
+def get_dataset_by_station_split(file, validation_size=0.25, test_size=0.2, replace=False):
+    dataset = pd.read_csv(file, low_memory=False)
+    dataset = dataset.drop_duplicates()
+    dataset = dataset[dataset["dst"]!='nrcan']
+    #dataset = dataset[dataset["dst"]!='Chesterfield']
+    #dataset = dataset[dataset["dst"]!='Wager']
+    dataset.dropna(how='all', inplace=True)
+    dataset.dropna(subset=['label'], inplace=True)
+
+    dataset['index'] = dataset.index
+    test_other_data = dataset[dataset["partition"].isin(["test"])]
+    training_data = dataset[dataset["partition"].isin(["train"])]
+
+    fn = lambda obj: obj.loc[np.random.choice(obj.index, round(len(obj.index) * test_size), replace), :]
+    test_same_idxs = training_data.groupby('label', as_index=False).apply(fn)['index'].tolist()
+    test_same_data = training_data[training_data['index'].isin(test_same_idxs)]
+
+    training_data = training_data[~training_data['index'].isin(test_same_idxs)]
+    fn = lambda obj: obj.loc[np.random.choice(obj.index, round(len(obj.index) * validation_size), replace), :]
+    validation_idxs = training_data.groupby('label', as_index=False).apply(fn)['index'].tolist()
+    validation_data = training_data[training_data['index'].isin(validation_idxs)]
+
+    training_data = training_data[~training_data['index'].isin(validation_idxs)]
+
+    print("training\n", training_data[['label']].value_counts())
+    print("validation\n", validation_data[['label']].value_counts())
+    print("test same\n", test_same_data[['label']].value_counts())
+    print("test other\n", test_other_data[['label']].value_counts())
+
+    return training_data, validation_data, test_same_data, test_other_data   
---
+++ solo_learn_train-bentho.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import argparse
+from argparse import Namespace
+
+import benthic_data_classes.definitions
+
+import json
+import os
+import torch
+from pytorch_lightning import Trainer
+from pytorch_lightning import loggers as pl_loggers
+from pytorch_lightning.callbacks import LearningRateMonitor
+from pytorch_lightning.strategies.ddp_spawn import DDPSpawnStrategy 
+from pytorch_lightning.strategies.ddp import DDPStrategy
+
+# Shakhboz's imports
+import torchvision.transforms as transforms
+import numpy as np
+
+# Solo-Learn import
+from solo.methods import BarlowTwins, SimCLR, MoCoV2Plus, MAE, SimSiam, BYOL, MoCoV3  # imports the method class
+from solo.utils.checkpointer import Checkpointer
+
+# and some utilities to perform data loading for the method itself, including augmentation pipelines
+
+from solo.data.pretrain_dataloader import (
+    prepare_n_crop_transform,
+    build_transform_pipeline,
+)
+
+from omegaconf import OmegaConf
+
+METHODS = {
+    "bt" : BarlowTwins,
+    "simclr": SimCLR,
+    "mocov2+": MoCoV2Plus,
+    "mocov3": MoCoV3,
+    "mae": MAE,
+    "simsiam": SimSiam,
+    "byol": BYOL
+}
+
+def main():
+    parser = argparse.ArgumentParser(description='Parameters for SSL benthic habitat project')
+    
+    # Required parameters
+    parser.add_argument('--ssl_cfg', type=str, required=True,
+                            help='set cfg file for SSL')                         
+    parser.add_argument('--aug_stack_cfg', type=str, required=True,
+                            help='set cfg file for augmentations')                        
+    parser.add_argument('--nodes', type=int, required=True,
+                            help='number of nodes') 
+    parser.add_argument('--gpus', type=int, required=True,
+                            help='number of gpus per node') 
+    parser.add_argument('--method', type=str, required=True,
+                            help='type of SSL method') 
+    # Other parameters
+    parser.add_argument('--mini', type=bool, default=False,
+                            help='use mini-dataset')
+    parser.add_argument('--seed', type=int, default=0,
+                            help='random seed (default: 0)')
+    parser.add_argument('--name', type=str, default='self-supervised_learning',
+                            help='set name for the run')
+
+    args = parser.parse_args()
+
+    seed = args.seed
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    root_dir = "/home/isaacxu/projects/rrg-ttt/become/benthicnet-compiled/compiled_250s_512px/"
+    csv_file_name = "dataset_2022-04-22.csv"
+    if args.mini:
+        csv_file_name = "mini-"+csv_file_name
+    csv_file_ssl = root_dir+csv_file_name
+    tar_dir = "/home/isaacxu/projects/rrg-ttt/become/benthicnet-compiled/compiled_labelled_512px/tar/"
+    lab_csv_file = "./Catami/WFdataset_subd3.csv"
+
+    _, validation_data, test_same_data, test_other_data = benthic_data_classes.definitions.get_dataset_by_station_split(lab_csv_file)
+
+    # common parameters for all methods
+    # some parameters for extra functionally are missing, but don't mind this for now.
+    
+    ssl_cfg_name = args.ssl_cfg
+    
+    with open("./ssl_cfgs/" + ssl_cfg_name) as f:
+        ssl_cfg = f.read()
+        
+    kwargs = json.loads(ssl_cfg)
+    cfg = OmegaConf.create(kwargs)
+
+    model = METHODS[args.method](cfg)
+
+    # we first prepare our single transformation pipeline
+    
+    aug_stack_name = args.aug_stack_cfg
+
+    with open("./ssl_cfgs/aug_stacks/" + aug_stack_name) as f:
+        aug_stack_cfg = f.read()
+        
+    transform_kwargs = json.loads(aug_stack_cfg)
+    transform_cfg = OmegaConf.create(transform_kwargs)
+
+    transform = build_transform_pipeline("custom", transform_cfg)
+
+    # then, we wrap the pipepline using this utility function
+    # to make it produce an arbitrary number of crops
+    transform = prepare_n_crop_transform([transform], num_crops_per_aug=[kwargs["data"]["num_large_crops"]])
+
+
+    train_dataset = benthic_data_classes.definitions.BenthicNetDatasetSSL(root_dir, csv_file_ssl, transform)
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=kwargs["optimizer"]["batch_size"],
+                                               shuffle=True,
+                                               pin_memory=True,
+                                               drop_last=True,
+                                               num_workers=kwargs["num_workers"])
+
+    val_transforms = transforms.Compose([
+         transforms.Resize(256),  # resize shorter
+         transforms.CenterCrop(224),  # take center crop
+         transforms.ToTensor(),
+         transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.228, 0.224, 0.225)),
+    ])
+
+    val_dataset = benthic_data_classes.definitions.BenthicNetDataset(tar_dir, validation_data, val_transforms)
+    val_loader = torch.utils.data.DataLoader(dataset=val_dataset,
+                                                batch_size=kwargs["optimizer"]["batch_size"],
+                                                shuffle=False,
+                                                pin_memory=True,
+                                                num_workers=kwargs["num_workers"])
+
+    os.environ['WANDB_NOTEBOOK_NAME'] = './solo_learn_train-bentho.ipynb'
+
+    run_name = args.name
+
+    wandb_logger = pl_loggers.WandbLogger(
+        name=run_name,  # name of the experiment
+        project="self-supervised_benthoscape",  # name of the wandb project
+        entity=None,
+        offline=False,
+    )
+    wandb_logger.watch(model, log="gradients", log_freq=100)
+
+    callbacks = []
+
+    # automatically log our learning rate
+    lr_monitor = LearningRateMonitor(logging_interval="epoch")
+    callbacks.append(lr_monitor)
+
+    # checkpointer can automatically log your parameters,
+    # but we need to wrap it on a Namespace object
+
+    ckpt_args = OmegaConf.create({"name": run_name})
+    # saves the checkout after every epoch
+
+    ckpt = Checkpointer(
+        ckpt_args,
+        logdir="./checkpoints/"+run_name,
+        frequency=kwargs["max_epochs"]-1,
+    )
+    callbacks.append(ckpt)
+
+    trainer_args = Namespace(**kwargs)
+
+    trainer = Trainer.from_argparse_args(
+        trainer_args,
+        logger=wandb_logger,
+        callbacks=callbacks,
+        strategy=DDPStrategy(find_unused_parameters=False),
+        accelerator="cuda",
+        num_nodes=args.nodes,
+        devices=args.gpus,
+        log_every_n_steps=200,
+    )
+
+    trainer.fit(model, train_loader, val_loader)
+
+if __name__ == "__main__":
+    main()
